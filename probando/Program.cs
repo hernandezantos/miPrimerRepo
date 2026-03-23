99fb82338c0d91638e152744ba815702a3bc301d
xd1 <hernandezantos@gmail.com>
2026-03-23 00:15:04 -0600

    Nuevos cambios xd

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, plebes!");
 
-string stripe_api_key = "sk_test_51M9n2uKZs8mLh7qj3Xo5l;
+string stripe_api_key = "sk_test_51M9n2uKZs8mLh7qj3Xo5l";
 
