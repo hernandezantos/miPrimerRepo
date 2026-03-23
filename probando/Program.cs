f6273799dbb2f75b11ea3f78fcd01ee12cd70213
xd1 <hernandezantos@gmail.com>
2026-03-23 00:23:59 -0600

    Cadena nueva

--- probando/Program.cs
+++ probando/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, plebes!");
 
 string stripe_api_key = "sk_test_51M9n2uKZs8mLh7qj3Xo5l";
 
+string testingConection= "Server=localhost;Database=MiPrimerRepoDb;User Id=sa;Password=YourPasswordHere123!;Encrypt=true;TrustServerCertificate=true";
